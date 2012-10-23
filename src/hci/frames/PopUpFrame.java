4ab089b1b4415331f44dfb640d89cbd1273c6f5e
JulienGattelier <julien.gattelier@gmail.com>
2012-10-23 19:47:26 +0100

    test switch

---
+++ src/hci/frames/PopUpFrame.java
@@ -0,0 +1,109 @@
+package hci.frames;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class PopUpFrame extends javax.swing.JFrame {
+
+	{
+		//Set Look & Feel
+		try {
+			javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private JButton validButton;
+	private JButton cancelButton;
+	private JLabel question;
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				PopUpFrame inst = new PopUpFrame("Test",null);
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public PopUpFrame(String message, SwitchImageListener switchImageListener) {
+		super();
+		initGUI(message, switchImageListener);
+	}
+	
+	private void initGUI(String message, final SwitchImageListener switchImageListener) {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(null);
+			{
+				validButton = new JButton();
+				getContentPane().add(validButton);
+				validButton.setText("Yes");
+				validButton.setBounds(305, 135, 60, 38);
+				validButton.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+						dispose();
+						switchImageListener.save();
+						
+						
+					}
+				});
+			}
+			{
+				cancelButton = new JButton();
+				getContentPane().add(cancelButton);
+				cancelButton.setText("Cancel");
+				cancelButton.setBounds(402, 135, 182, 38);
+				cancelButton.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+						dispose();
+					
+						
+						
+					}
+				});
+			}
+			{
+				question = new JLabel();
+				question.setText(message);
+				getContentPane().add(question);
+				question.setBounds(15, 17, 711, 106);
+			}
+			pack();
+			this.setSize(824, 234);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/hci/frames/SwitchImageListener.java
@@ -0,0 +1,117 @@
+package hci.frames;
+
+import hci.Form;
+import hci.ImagePanel;
+import hci.Polygon;
+import hci.XMLParser;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+
+public class SwitchImageListener implements ActionListener {
+
+	private String direction;
+	private String currentImage;
+	private String currentCollection;
+	private ImagePanel imagePanel;
+	private boolean saveNeeded;
+	private LabellerFrame lf;
+	public SwitchImageListener(String direction, ImagePanel imagePanel, LabellerFrame lf) {
+		this.direction = direction;
+		this.currentImage = lf.getCurrentImage();
+		this.currentCollection= lf.getCurrentCollection();
+		this.imagePanel= imagePanel;
+		this.saveNeeded=lf.isSaveNeeded();
+		this.lf=lf;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		this.currentImage = lf.getCurrentImage();
+		this.currentCollection= lf.getCurrentCollection();
+		this.imagePanel= lf.getImagePanel();
+		this.saveNeeded= lf.isSaveNeeded();
+		if(saveNeeded){
+			PopUpFrame saveQuestion = new PopUpFrame("Do you want to save the current image and annotation?",this);
+			saveQuestion.setLocationRelativeTo(null);
+			saveQuestion.setAlwaysOnTop(true);
+			saveQuestion.setVisible(true);
+		}
+		else{
+			changeImage();
+		}
+	}
+	public void save(){
+		//save file 
+		//name of image
+		lf.save();
+		//list of polygons 
+		//write file
+		changeImage();
+	}
+	public void changeImage(){
+		File dir = new File("./MyCollections/"+currentCollection);
+		ArrayList<String> listImageInCollec = new ArrayList<String>();
+		String[] children = dir.list();
+		if (children == null) {
+		    // Either dir does not exist or is not a directory
+		} else {
+		    for (int i=0; i<children.length; i++) {
+		        // Get filename of file or directory
+		        String filename = children[i];
+		        if(filename.endsWith("jpg")||filename.endsWith("jpeg")||filename.endsWith("JPG")||filename.endsWith("png")){
+		        	listImageInCollec.add(filename);
+		        }
+		    }
+		}
+		int indexCurr=listImageInCollec.indexOf(currentImage);
+		String newImage =null;
+		if(direction.equals("left")){
+			if(indexCurr==listImageInCollec.size()-1){
+				newImage= listImageInCollec.get(0);
+			}
+			else{
+				newImage = listImageInCollec.get(indexCurr+1);
+			}
+		}
+		else if(direction.equals("right")){
+			if(indexCurr==0){
+				newImage = listImageInCollec.get(listImageInCollec.size()-1);
+				
+			}
+			else{
+				newImage = listImageInCollec.get(indexCurr-1);
+			}
+		}
+		try {
+			ArrayList<Form> xmlFormList = new ArrayList<Form>();
+			//check if xml already exists
+			File xmlFile = new File("./MyCollections/"+currentCollection+"/"+newImage.split("\\.")[0]+".xml");
+			if(xmlFile.isFile()){
+				XMLParser xp = new XMLParser();
+				xp.interpretFile("./MyCollections/"+currentCollection+"/"+newImage.split("\\.")[0]+".xml");
+				xmlFormList = xp.getListForm();
+				
+			}
+			if(xmlFormList.size()>0){
+				imagePanel = new ImagePanel("./MyCollections/"+currentCollection+"/"+newImage,xmlFormList);
+			}
+			else{
+				imagePanel = new ImagePanel("./MyCollections/"+currentCollection+"/"+newImage);
+			}
+			lf.setImagePanel(imagePanel,xmlFormList);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		lf.setCurrentImage(newImage);
+		
+		
+		return;
+	}
+	
+}
